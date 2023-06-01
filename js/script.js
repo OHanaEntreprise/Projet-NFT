6e5f7423725d4f7348e9c3c96f3291082fb56c16
Picasso004 <sanvee34@gmail.com>
2023-06-01 16:36:06 +0200

    Countdown attempt

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// Change the targetDate to the desired countdown date
+const targetDate = new Date('2023-07-01T00:00:00');
+
+function updateCountdown() {
+    const now = new Date();
+    const timeDifference = targetDate - now;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById('days').textContent = days.toString().padStart(2, '0');
+    document.getElementById('hours').textContent = hours.toString().padStart(2, '0');
+    document.getElementById('minutes').textContent = minutes.toString().padStart(2, '0');
+    document.getElementById('seconds').textContent = seconds.toString().padStart(2, '0');
+}
+
+// Initial countdown update
+updateCountdown();
+
+// Update countdown every second
+setInterval(updateCountdown, 1000);
